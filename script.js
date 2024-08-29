549b22d6be51c09a8c8783a755860af08eac0c40
cristian-nambo <117240620+cristian-nambo@users.noreply.github.com>
2024-08-29 02:59:43 +0000

    Fixed bugs on html and js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-getElementById('funfactButton').addEventListener('click', function() {
-  const funFact = document.getElementById('funfact');
+document.getElementById('funFactButton').addEventListener('click', function() {
+  const funFact = document.getElementById('funFact');
   if (funFact.style.display === 'none') {
       funFact.style.display = 'block';
   } else {
       funFact.style.display = 'none';
   }
 });
+
+// document.getElementById('funfactButton').addEventListener('click', function() {
+//   const funFact = document.getElementById('funfact');
+//   if (funFact.style.display === 'none' || funFact.style.display === '') {
+//       funFact.style.display = 'block';
+//   } else {
+//       funFact.style.display = 'none';
+//   }
+// });
+
